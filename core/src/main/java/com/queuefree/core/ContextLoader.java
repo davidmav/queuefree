b5864d75872e701f99575ac2536112c120526ad0
davidm <davidm@davidmavashev.INTELLINX-SW.COM>
2014-02-23 15:43:49 -0500

    Added a shutdown hook

--- core/src/main/java/com/queuefree/core/ContextLoader.java
+++ core/src/main/java/com/queuefree/core/ContextLoader.java
@@ -1,16 +1,38 @@
 package com.queuefree.core;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * Hello world!
- * 
- */
 public class ContextLoader {
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext(
+		final ApplicationContext context = new ClassPathXmlApplicationContext(
 				"META-INF/applicationContext.xml");
+		
+		waitForShutdown(context);
+		
 
+	}
+
+	private static void waitForShutdown(final ApplicationContext context) {
+		Runtime.getRuntime().addShutdownHook(new Thread()
+        {
+            @Override
+            public void run()
+            {
+            	synchronized (context) {
+            		((ConfigurableApplicationContext)context).close();
+            		context.notifyAll();
+				}
+            }
+        });
+
+		try {
+			synchronized (context) {
+				context.wait();
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 }
